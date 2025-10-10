42be325572b0b1d2be16e1719f3a59362e8e643c
bhuvan1411 <bhuvang1411@gmail.com>
2025-10-10 13:24:12 +0530

    channels

--- channels/models.py
+++ channels/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your models here.
+class Channel(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('channels:detail', kwargs={'pk': self.pk})
--- channels/views.py
+++ channels/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
+from .models import Channel
 
-# Create your views here.
+class ChannelListView(ListView):
+    model = Channel
+    template_name = 'channels/list.html'
+
+class ChannelDetailView(DetailView):
+    model = Channel
+    template_name = 'channels/detail.html'
+
+class ChannelCreateView(LoginRequiredMixin, CreateView):
+    model = Channel
+    fields = ['name', 'description']
+    template_name = 'channels/form.html'
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+class ChannelUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Channel
+    fields = ['name', 'description']
+    template_name = 'channels/form.html'
+
+    def test_func(self):
+        return self.request.user == self.get_object().owner
+
+class ChannelDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Channel
+    template_name = 'channels/confirm_delete.html'
+    success_url = reverse_lazy('channels:list')
+
+    def test_func(self):
+        return self.request.user == self.get_object().owner
