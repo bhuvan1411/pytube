1aeb678f7d938f2234b1007b841fc0d5e9216be4
Bhagya Nandini <pbhagyanandini@gmail.com>
2025-10-10 17:09:20 +0530

    Added login and register forms in accounts

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .forms import RegisterForm, LoginForm
 
-# Create your views here.
+
+def register_view(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            return redirect('accounts:login')
+    else:
+        form = RegisterForm()
+    return render(request, 'accounts/register.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                form.add_error(None,'Invalid username or password')
+    else:
+        form = LoginForm()
+    return render(request, 'accounts/login.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    #return redirect('accounts:login')
+    return render(request, 'accounts/logout.html') 
+
+
+
+
+
+
+
+
+
+
+
+
+# class ForgotPasswordView(PasswordResetView):
+#     form_class = ForgotPasswordForm
+#     template_name = 'accounts/password_reset.html'
+#     success_url = reverse_lazy('password_reset_done')
